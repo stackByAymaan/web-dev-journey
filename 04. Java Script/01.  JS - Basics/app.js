4c87a30d9a584197730e130bbedb8c72598c88ea
stackByAymaan <aymaanlenovoloq@gmail.com>
2026-08-18 23:34:10 +0530

    Added Chap 1 - Basics

---
+++ 04. Java Script/01.  JS - Basics/app.js
@@ -0,0 +1,14 @@
+//* BASICS
+
+console.log("Hello World");
+let a = 10;
+let b = 5;
+// let output = "the total price is : " + (a + b) + ' RUPEES. ';
+ let output = `The total price is : ${a + b} rupees`;
+console.log(output);
+
+// More example
+let c = 10;
+let d = 5;
+let output2 = `Sum: ${c + d}`;
+console.log(output2);
