4ca0f97bd25ee4b6e7ae9e20ca4a06363bf21304
stackByAymaan <aymaanlenovoloq@gmail.com>
2026-08-27 21:06:32 +0530

    MongoDB Setup

---
+++ 12. MongoDB/index.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');  
+// Import mongoose library
+
+mongoose.connect('mongodb://127.0.0.1:27017/test');  
+// Connect to MongoDB database named "test" running locally
+
+
