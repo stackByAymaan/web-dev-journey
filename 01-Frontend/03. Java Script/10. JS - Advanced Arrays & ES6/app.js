acff5965fc0288fefd3ac21c6e308ad50beabc3b
stackByAymaan <aymaanlenovoloq@gmail.com>
2026-08-31 00:15:16 +0530

    Update app.js

--- 01-Frontend/03. Java Script/10. JS - Advanced Arrays & ES6/app.js
+++ 01-Frontend/03. Java Script/10. JS - Advanced Arrays & ES6/app.js
@@ -257,3 +257,35 @@ console.log(obj);
 
 
 console.log(" ");
+
+
+
+
+//^ Rest
+
+function sum(...args) {
+    for(let i = 0; i < args.length; i++) {
+        console.log("you gave us:", args[i]);
+    }
+}
+
+sum(1, 2, 3, 4, 5);
+
+
+console.log(" ");
+
+
+function min(...args) {
+    return args.reduce((min, el) => {
+        if(min > el) {
+            return el;
+        } else {
+            return min;
+        }
+    });
+}
+
+console.log(min(2, 5, 1, 8, 3));
+
+
+console.log(" ");
