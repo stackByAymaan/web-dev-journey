44e73be8f8a4f97bf08ac09f2d8c996e1f11c729
stackByAymaan <aymaanlenovoloq@gmail.com>
2026-09-04 15:49:42 +0530

    Constraints

--- 03-Database/01.SQL/Sql_Basics.sql
+++ 03-Database/01.SQL/Sql_Basics.sql
@@ -45,3 +45,18 @@ CREATE TABLE datatype_demo (
     dob DATE,
     birth_year YEAR
 );
+
+
+--^ Constraints + First Main Table
+
+CREATE TABLE user (
+    id INT PRIMARY KEY,
+    age INT,
+    name VARCHAR(30) NOT NULL,
+    email VARCHAR(50) UNIQUE,
+    followers INT DEFAULT 0,
+    following INT,
+    CONSTRAINT CHECK (age >= 13)
+);
+
+SHOW TABLES;
