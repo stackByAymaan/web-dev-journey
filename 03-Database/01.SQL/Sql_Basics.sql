2639506bdbe26e9cc92de101d2eda0e2b2ac8e2b
stackByAymaan <aymaanlenovoloq@gmail.com>
2026-09-04 15:48:42 +0530

    Data Types

--- 03-Database/01.SQL/Sql_Basics.sql
+++ 03-Database/01.SQL/Sql_Basics.sql
@@ -23,3 +23,25 @@ VALUES
 (102, "bob", 14);
 
 SELECT * FROM student;
+
+
+
+--^ Data Types
+
+-- CHAR / VARCHAR / BLOB / INT / TINYINT / BIGINT
+-- BIT / FLOAT / DOUBLE / BOOLEAN / DATE / YEAR
+
+CREATE TABLE datatype_demo (
+    fixed_name CHAR(10),
+    name VARCHAR(30),
+    data BLOB,
+    age INT,
+    small_num TINYINT,
+    large_num BIGINT,
+    bits BIT(2),
+    price FLOAT,
+    salary DOUBLE,
+    active BOOLEAN,
+    dob DATE,
+    birth_year YEAR
+);
