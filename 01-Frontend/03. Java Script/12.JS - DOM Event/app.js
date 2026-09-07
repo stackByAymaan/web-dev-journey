e769d00796db30697cf9ccfb22a7abed7d441d6f
stackByAymaan <aymaanlenovoloq@gmail.com>
2026-09-07 20:33:49 +0530

    Keybord Events

--- 01-Frontend/03. Java Script/12.JS - DOM Event/app.js
+++ 01-Frontend/03. Java Script/12.JS - DOM Event/app.js
@@ -1,90 +1,18 @@
-
-//Multiple Click/work on single button 
-// let btns = document.querySelectorAll("Button");
-
-// for (btn of btns) {
-//     btn.onclick = sayHello;
-//     btn.onmouseenter = function () {
-//      console.log("you entred a button");
-//     };
-//     console.dir(btn);
-// }
-// function sayHello() {
-//     alert("hellooo");
-// }
-
-
-
-// Event listner
-let btns = document.querySelectorAll(".clkBtn");
-
-for (btn of btns) {
-    //  btn.addEventListener("click", sayHello);
-    //  btn.addEventListener("click", sayName);
-     btn.addEventListener("dblclick", function(){  //double click
-        console.log("you double clicked");
-     });
+function changeColor() {
+    console.dir(this.innerText);
+    this.style.backgroundColor = "blue";
 }
 
-function sayHello() {
-    alert("hellooo");
-}
-
-function sayName() {
-    console.log("aymaan");
-}
+Thisbtn.addEventListener("click", changeColor);
+h1.addEventListener("click", changeColor);
 
 
 
-//practice
-let colorBtn = document.querySelector(".btn");
+//Keybord Events
+let Keybtn = document.querySelector(".Keybtn");
 
-colorBtn.addEventListener("click", function () {
-    let h3 = document.querySelector("h3");
-    let randomColor = getRandomColor();
-    h3.innerText = randomColor;
-
-
-    let div = document.querySelector("div");
-    div.style.backgroundColor = randomColor;
-
-    console.log("color updated");
+// click
+Keybtn.addEventListener("click", function (event){
+    console.log(event);
+    console.log("Button Clicked");
 });
-
-function getRandomColor() {
-    let red = Math.floor(Math.random() * 255);
-    let green = Math.floor(Math.random() * 255);
-    let blue = Math.floor(Math.random() * 255);
-
-    let color = `rgb(${red}, ${green}, ${blue})`;
-    return color;
-}
-
-
-//Event listner for Element
-let p = document.querySelector("p");
-
-p.addEventListener("click", function () {
-    console.log("para was clicked");
-});
-
-let box = document.querySelector(".box");
-box.addEventListener("mouseenter", function() {
-    console.log("mouse inside div");
-});
-
-
-//This with event listner 
-let Thisbtn = document.querySelector(".Thisbtn");
-let h1 = document.querySelector("h1")
-
-// Thisbtn.addEventListener("click", function(){
-//     console.dir(this.innerText);
-//     this.style.background = 'blue';
-// });
-
-
-// h1.addEventListener("click", function(){
-//     console.dir(this.innerText);
-//     this.style.background = 'blue';
-// });
