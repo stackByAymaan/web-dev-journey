5059d94e8371e2d64e01c136528887e01e5a92b6
stackByAymaan <aymaanlenovoloq@gmail.com>
2026-09-09 20:13:28 +0530

    Important theory

--- 01-Frontend/03. Java Script/13. JS - Call Stack, Async JS and Promises/app.js
+++ 01-Frontend/03. Java Script/13. JS - Call Stack, Async JS and Promises/app.js
@@ -30,3 +30,17 @@ function three() {
 }
 
 three();
+
+//* breakpoint - Breakpoints allow us to pause code execution at a specific line and inspect variables, function calls, and program flow step by step for debugging.
+
+
+
+
+//* JS is single Threaded -  JavaScript is a single-threaded language because it executes one task at a time using a single call stack, handling tasks sequentially rather than running multiple pieces of code simultaneously.
+
+
+
+//* JavaScript is a single-threaded language, but functions like setTimeout() do not run on the JavaScript thread. They are handled by browser APIs (implemented in languages such as C++). When the timer finishes, the browser sends the callback to the callback queue, and the Event Loop moves it to the Call Stack when the stack becomes empty. Therefore, multiple setTimeout() calls can work concurrently even though JavaScript itself is single-threaded.
+
+
+
