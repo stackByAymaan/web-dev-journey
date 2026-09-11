84dc9181f5ba7fb9fd5a09c73fc3e00d867ae7be
stackByAymaan <aymaanlenovoloq@gmail.com>
2026-09-11 16:26:27 +0530

    throw keyword

--- 01-Frontend/03. Java Script/14. JS - Async Await, APIs, HTTP & Fetch/app.js
+++ 01-Frontend/03. Java Script/14. JS - Async Await, APIs, HTTP & Fetch/app.js
@@ -14,3 +14,18 @@ console.log(greet());
 
 
 console.log(" ");
+
+
+
+//throw keyword
+async function greet1() {
+    throw "some random error";
+    return "hello !!";
+}
+greet1()   
+.then((result) => {                    //Using Then and catch
+    console.log("promised was resolved", result);
+})      
+.catch((err) => {
+    console.log("promise was rejected wit err : ", err);
+});
