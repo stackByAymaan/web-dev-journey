5f741416d440a1d1e7223b1f1f136dbd9de14118
stackByAymaan <aymaanlenovoloq@gmail.com>
2026-09-11 16:28:05 +0530

    /reffering old code

--- 01-Frontend/03. Java Script/14. JS - Async Await, APIs, HTTP & Fetch/app.js
+++ 01-Frontend/03. Java Script/14. JS - Async Await, APIs, HTTP & Fetch/app.js
@@ -61,3 +61,29 @@ async function demo() {
 }
 
 demo();
+
+
+
+//reffering old code
+// let h1 = document.querySelector("h1");
+
+// function changeColor(color, delay) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             h1.style.color = color;
+//             console.log(`color changed to ${color}!`);
+//             resolve("color changed!");
+//         }, delay);
+//     });
+// }
+
+// async function demo() {
+//     await changeColor("red", 1000);
+//     await changeColor("orange", 1000);
+//     await changeColor("green", 1000);
+//     await changeColor("blue", 1000);
+
+//     console.log("all colors changed");
+// }
+
+// demo();
