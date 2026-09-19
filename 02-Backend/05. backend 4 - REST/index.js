eb4023af8312ca5020c3c39922810036ab76eab7
stackByAymaan <aymaanlenovoloq@gmail.com>
2026-09-19 23:06:16 +0530

    Views setup

--- 02-Backend/05. backend 4 - REST/index.js
+++ 02-Backend/05. backend 4 - REST/index.js
@@ -3,6 +3,12 @@ const app = express();
 const port = 8080;
 
 
+
+app.set("views engine", "ejs");
+app.set("views", Path2D.json(__dirname, "public"));
+
+
+
 app.listen(port, () => {
     console.log("listening to port : 8080");
 });
