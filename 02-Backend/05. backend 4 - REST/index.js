945009754a0cf3b156539f6cb78aebbfef976104
stackByAymaan <aymaanlenovoloq@gmail.com>
2026-09-19 23:07:14 +0530

    Public

--- 02-Backend/05. backend 4 - REST/index.js
+++ 02-Backend/05. backend 4 - REST/index.js
@@ -7,7 +7,7 @@ const port = 8080;
 app.set("views engine", "ejs");
 app.set("views", Path2D.json(__dirname, "public"));
 
-
+app.set(express.static(Path2D.join(__dirname, "public")));
 
 app.listen(port, () => {
     console.log("listening to port : 8080");
