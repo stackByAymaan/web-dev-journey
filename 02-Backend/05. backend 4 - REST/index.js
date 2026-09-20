bb7e5209d756a7aa46d6a2c6b3c37579c2f270f2
stackByAymaan <aymaanlenovoloq@gmail.com>
2026-09-20 15:46:38 +0530

    implementeed Patch

--- 02-Backend/05. backend 4 - REST/index.js
+++ 02-Backend/05. backend 4 - REST/index.js
@@ -1,28 +1,31 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-
 const port = 8080;
+const { v4: uuidv4 } = require("uuid");
+
+
+app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({ extended: true }));
+
 
 let posts = [
     {
-        id:"1a",
+        id: uuidv4(),
         username :"apnacollege",
         content : "I love coding"
     },
     {
-        id:"2b",
+        id: uuidv4(),
         username :"Aymaan",
         content : "Aymaan is trash , he got reject in his 1st internship"
     },
     {
-        id:"3b",
+        id: uuidv4(),
         username :"modi",
         content : "PM of india"
     }
@@ -39,6 +42,14 @@ app.get("/posts/new", (req, res) => {
 });
 
 
+
+app.post("/posts", (req, res) => {
+    let { username, content } = req.body;
+    let newId = uuidv4();
+    posts.push({ id, username, content });
+    res.redirect("/posts");
+});
+
 app.get("/posts/:id", (req, res) => {
     let {id} = req.params;
     let post= posts.find((p) => id === p.id);
@@ -48,11 +59,10 @@ app.get("/posts/:id", (req, res) => {
     // res.send("request working");
 });
 
-
-app.post("/posts", (req, res) => {
-    let { username, content } = req.body;
-    posts.push({ username, content });
-    res.redirect("/posts");
+app.patch("/posts/:id" , (req, res) => {
+    let { id } = req.params;
+    console.log(id);
+    res.send("patch request working");
 });
 
 app.listen(port, () => {
