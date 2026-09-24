8dc4c2b91b0ec8941c3f2d52c2864c47527705e5
stackByAymaan <aymaanlenovoloq@gmail.com>
2026-09-24 23:50:05 +0530

    Inseting multiple user

--- 02-Backend/06 Backend 5 - Node with SQL/index.js
+++ 02-Backend/06 Backend 5 - Node with SQL/index.js
@@ -11,8 +11,18 @@ const connection = mysql.createConnection({
 });
 
 //* Inserting New Data
-let q ="INSERT INTO user (id, username, email, password) VALUES ( ?, ?, ?, ?)";
-let user = ["123", "123_newuser", "abc@gmail.com", "abc"];
+// let q ="INSERT INTO user (id, username, email, password) VALUES (?,?,?,?)";
+// let user1 = ["123", "123_newuser", "abc@gmail.com", "abc"];
+
+//Inserting multiple data
+let q ="INSERT INTO user (id, username, email, password) VALUES (?)";
+let users = [
+    ["101", "aymaan_dev", "aymaan@gmail.com", "pass101"],
+    ["102", "anish_codes", "anish@gmail.com", "pass102"],
+    ["103", "imroj_web", "imroj@gmail.com", "pass103"],
+    ["104", "rahul_dev", "rahul@gmail.com", "pass104"],
+    ["105", "sneha_js", "sneha@gmail.com", "pass105"],
+];
 
 
 try {
