20f4a3e16297a3acb0f348119e6d8a7f3ae0c3fa
stackByAymaan <aymaanlenovoloq@gmail.com>
2026-09-24 23:22:27 +0530

    Running from terminal

--- 02-Backend/06 Backend 5 - Node with SQL/index.js
+++ 02-Backend/06 Backend 5 - Node with SQL/index.js
@@ -13,7 +13,10 @@ const connection = mysql.createConnection({
 try {
     connection.query("SHOW TABLES", (err, result) => {
         if (err) throw err;
-        console.log(result)
+        console.log(result);
+        console.log(result.length);
+        console.log(result[0]);
+        console.log(result[1]);
     });
 } catch (err) {
     console.log(err);
